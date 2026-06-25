94f8bae7ff6bdd114a7cd0c6b855995aa102ea38
RG-67 <rittikghoshkolkata@gmail.com>
2026-06-25 21:40:17 +0530

    innerPading added

--- app/src/main/java/com/project/autoandroid/MainActivity.kt
+++ app/src/main/java/com/project/autoandroid/MainActivity.kt
@@ -20,7 +20,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             AutoAndroidTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-
+                    innerPadding.calculateTopPadding()
                 }
             }
         }
