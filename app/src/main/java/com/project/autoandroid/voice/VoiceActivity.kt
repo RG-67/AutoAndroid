027b18b6613bbb680e3df5cbe5516773de87ef07
Misbahul Kadery <kaderymisbahul@gmail.com>
2026-06-27 18:53:04 +0530

    voice layout created

---
+++ app/src/main/java/com/project/autoandroid/voice/VoiceActivity.kt
@@ -0,0 +1,83 @@
+package com.project.autoandroid.voice
+
+import android.graphics.Paint.Align
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
+import com.project.autoandroid.R
+import com.project.autoandroid.ui.theme.AutoAndroidTheme
+
+
+class VoiceActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AutoAndroidTheme {
+                VoiceUi(Modifier)
+            }
+        }
+    }
+
+
+    @Composable
+    fun VoiceUi(modifier: Modifier = Modifier) {
+        Column(
+            modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                modifier = Modifier
+                    .width(50.dp)
+                    .height(50.dp)
+                    .clip(CircleShape)
+                    .clickable {
+                        Toast.makeText(this@VoiceActivity, "Image clicked", Toast.LENGTH_SHORT)
+                            .show()
+                    },
+                contentDescription = getString(R.string.app_name),
+                painter = painterResource(R.drawable.mic)
+            )
+
+            Text(
+                modifier = Modifier.padding(10.dp),
+                text = "TEXT",
+                style = TextStyle(
+                    fontSize = TextUnit.Unspecified,
+                    color = Color.Black,
+                    textAlign = TextAlign.Center,
+                    fontWeight = FontWeight.Bold,
+                )
+            )
+        }
+    }
+
+
+}
