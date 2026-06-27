5916f67aa74a297e2dcd868fcbe4dafaff9c5caf
Misbahul Kadery <kaderymisbahul@gmail.com>
2026-06-27 16:06:46 +0530

    packages added

--- app/src/main/java/com/project/autoandroid/voice/MainActivity.kt
+++ app/src/main/java/com/project/autoandroid/voice/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.project.autoandroid
+package com.project.autoandroid.voice
 
 import android.os.Bundle
 import android.util.Log
@@ -6,12 +6,8 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.project.autoandroid.ui.theme.AutoAndroidTheme
 
 class MainActivity : ComponentActivity() {
